7036a34ede32846dc7c2fc2182d113f19a6ed8b4
gouvina-fing <german.ouvina@fing.edu.uy>
2021-04-10 20:03:45 -0300

    Comments in ej1

--- lab1/ej1.pl
+++ lab1/ej1.pl
@@ -5,26 +5,25 @@
 % Predicados auxiliares
 % --------------------------
 
-% pertenece(?X,?L) - Comprueba si el elemento X pertenece a la lista L
-pertenece(X,[X|_]).
-pertenece(X,[_|T]):- pertenece(X,T).
+% pertenece(?X, ?L) - Comprueba si el elemento X pertenece a la lista L
+pertenece(X, [X|_]).
+pertenece(X, [_|T]):- pertenece(X,T).
 
-% largo(?L,?N) - 
-largo([], 0).
-largo([_|T], X) :- largo(T, X2), X is X2 + 1.
-
-% pertenece(?X,?L) - 
+% crear_fila(+N, ?E, ?F) - Comprueba si la lista F tiene largo N y E elementos
 crear_fila(1, E, [E]).
 crear_fila(N, E, [E|T]) :- N > 1, N2 is N-1, crear_fila(N2, E, T).
 
-% pertenece(?X,?L) - 
+% enesimo(+L, +N, ?X) - Comprueba si X es el N-ésimo elemento de la lista L
 enesimo([H|_], 1, H).
 enesimo([_|T], N, X) :- M is N-1, enesimo(T, M, X).
 
-% pertenece(?X,?L) - 
-generar_transpuesta(M, L, L, [C]) :- col(L, M, C).
-generar_transpuesta(M, N, L, [C1|C2]) :- col(N, M, C1), N2 is N+1, generar_transpuesta(M, N2, L, C2).
+% generar_transpuesta(+A, +L, +N, ?C) - Genera matriz transpuesta A a partir de matriz C, siendo L un acumulador para iterar y N la cantidad de filas de C
+generar_transpuesta(A, N, N, [C]) :- col(L, A, C).
+generar_transpuesta(A, L, N, [C1|C2]) :- col(L, A, C1), L2 is L+1, generar_transpuesta(A, L2, N, C2).
 
+% largo(+L, ?N) - Comprueba si la lista L tiene N elementos
+largo([], 0).
+largo([_|T], X) :- largo(T, X2), X is X2 + 1.
 
 % Predicados principales
 % --------------------------
@@ -48,23 +47,19 @@ suma([H|T], X) :- suma(T, X2), X is H + X2.
 matriz(1, N, E, [F]) :- crear_fila(N, E, F).
 matriz(M, N, E, [F|T2]) :- M > 1, crear_fila(N, E, F), M2 is M-1, matriz(M2, N, E, T2).
 
-
 % valor_celda(+I,+J,+A,?E) - E es el contenido de la celda (I,J) de la matriz A
 valor_celda(1, J, [H|_], E) :- enesimo(H, J, E).
 valor_celda(I, J, [_|T], E) :- M is I-1, valor_celda(M, J, T, E).
 
-% OTRA FORMA
-% valor_celda(I,J,A,E) :- fila(I,A,F), enesimo(F, J, E).
-
-% fila(?N,+M,?F) - F es la fila N-ésima de la matriz
+% fila(+N, +M, +F) - F es la fila N-ésima de la matriz
 fila(1, [H|_], H).
 fila(N, [_|T], F) :- M is N-1, fila(T, M, F).
 
-% col(?N, +M,?C) - C es la columna N-ésima de la matriz
+% col(+N, +M, ?C) - C es la columna N-ésima de la matriz
 col( N, [H], [X]) :- enesimo(H, N, X).
 col( N, [H|T], [C1|C2]) :- enesimo(H, N, C1), col(T, N, C2).
 
-% transpuesta(?M, ?T) - T es la matriz transpuesta de M
+% transpuesta(+M, ?T) - T es la matriz transpuesta de M
 transpuesta([[]], [[]]).
 transpuesta([H|T], X) :- largo(H, N), generar_transpuesta([H|T], 1, N, X).
 
