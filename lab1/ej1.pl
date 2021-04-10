42289fa0f9c31108adac18418125122617403f7b
gouvina-fing <german.ouvina@fing.edu.uy>
2021-04-10 18:27:51 -0300

    Arreglo ej1 - matriz

--- lab1/ej1.pl
+++ lab1/ej1.pl
@@ -5,23 +5,22 @@
 % Predicados auxiliares
 % --------------------------
 
+% pertenece(?X,?L) - 
+pertenece(X,[X|_]).
+pertenece(X,[_|T]):- pertenece(X,T).
+
 largo([], 0).
 largo([_|T], X) :- largo(T, X2), X is X2 + 1.
+
+crear_fila(1, E, [E]).
+crear_fila(N, E, [E|T]) :- N > 1, N2 is N-1, crear_fila(N2, E, T).
 
 enesimo([H|_], 1, H).
 enesimo([_|T], N, X) :- M is N-1, enesimo(T, M, X).
 
-agregar_atras([], X, [X]).  
-agregar_atras([H|T], X, [H|L]) :- agregar_atras(T, X, L).
-
-agregar_adelante([], X, [X]).
-agregar_adelante([H|T], X, [X,H|T]).
-
 generar_transpuesta(M, L, L, [C]) :- col(L, M, C).
 generar_transpuesta(M, N, L, [C1|C2]) :- col(N, M, C1), N2 is N+1, generar_transpuesta(M, N2, L, C2).
 
-pertenece(X,[X|_]).
-pertenece(X,[_|T]):- pertenece(X,T).
 
 % Predicados principales
 % --------------------------
@@ -40,10 +39,11 @@ elegirN([H|T], L1, N, L2) :- elegir(H, L1, L), M is N-1, elegirN(T, L, M, L2).
 suma([], 0).
 suma([H|T], X) :- suma(T, X2), X is H + X2.
 
-% matriz(+M,+N,?A) - A es una matriz de M filas y N columnas. La matriz se representa mediante una lista de M filas, donde cada fila es una lista de N celdas.
-matriz(0, 0, [[]]).
-matriz(1, N, [[H|T]]) :- largo([H|T], N).
-matriz(M, N, [[H1|T1]|T2]) :- largo([H1|T1], N), M2 is M-1, matriz(M2, N, T2).
+% matriz(+M,+N, +E, ?A) - A es una matriz de M filas y N columnas, donde cada celda tiene el elemento E.
+% La matriz se representa mediante una lista de M filas, donde cada fila es una lista de N celdas.
+matriz(1, N, E, [F]) :- crear_fila(N, E, F).
+matriz(M, N, E, [F|T2]) :- M > 1, crear_fila(N, E, F), M2 is M-1, matriz(M2, N, E, T2).
+
 
 % valor_celda(+I,+J,+A,?E) - E es el contenido de la celda (I,J) de la matriz A
 valor_celda(1, J, [H|_], E) :- enesimo(H, J, E).
@@ -57,12 +57,8 @@ fila(1, [H|_], H).
 fila(N, [_|T], F) :- M is N-1, fila(T, M, F).
 
 % col(?N, +M,?C) - C es la columna N-ésima de la matriz
-col(N, [H], C) :- enesimo(H, N, X), agregar_adelante([], X, C).
-col(N, [H|T], C) :- col(N, T, C2), enesimo(H, N, X), agregar_adelante(C2, X, C).
-
-% OTRA FORMA
-% col( N, [H], [X]) :- enesimo(H, N, X).
-% col( N, [H|T], [C1|C2]) :- enesimo(H, N, C1), col(T, N, C2).
+col( N, [H], [X]) :- enesimo(H, N, X).
+col( N, [H|T], [C1|C2]) :- enesimo(H, N, C1), col(T, N, C2).
 
 % transpuesta(?M, ?T) - T es la matriz transpuesta de M
 transpuesta([[]], [[]]).
