c4fbefa53d3e87384a58a6693adc7671b09ac2b4
gouvina-fing <german.ouvina@fing.edu.uy>
2021-04-18 20:22:42 -0300

    Agregado informe, entrega, pruebas finales y otras cositas

--- lab1/tests/comparacionEj3.pl
+++
@@ -1,9 +0,0 @@
-% ej3.pl
-time((caminito(3,[],pos(1,1),pos(3,3),Caminito), fail; true)).
-% 2,659 inferences, 0.000 CPU in 0.000 seconds (100% CPU, 6554556 Lips)
-true.
-
-% ej3-NoEff.pl
-?- time((caminito(3,[],pos(1,1),pos(3,3),Caminito), fail; true)).
-% 199,746 inferences, 0.030 CPU in 0.030 seconds (100% CPU, 6682336 Lips)
-true.
