06b9a3f34c161bf55c4f3da9a0f989f2a49a5fdb
gmarco10 <gonzamarco97@gmail.com>
2021-05-08 19:11:08 -0300

    Fix Reentrega

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -64,28 +64,10 @@ numeros(I,F,[I|L]) :- I < F, I2 is I+1, numeros(I2, F, L).
 % Predicados auxiliares
 % --------------------------
 
-% borrarH(+H, +L1, ?L2) <- L2 resulta de eliminar todas las ocurrencias H de la lista L1
-borrarH(_, [], []). 
-borrarH(H, [H|L1], L2) :- borrarH(H, L1, L2).
-borrarH(H, [Y|L1], [Y|L2]) :- Y \= H, borrarH(H, L1, L2).
-
-% borrar(+L, +L1, ?L2) <- L2 resulta de eliminar los elementos de L que se encuentren en L1
-borrar([], L, L).
-borrar([H|L], L1, L2) :- borrarH(H, L1, L3), borrar(L, L3, L2).
-
-% calculo_multiplos(+H, +N, +L, ?L2) <- L2 es la lista de todos los multiplos de H, mayores a H y menores a N 
-calculo_multiplos(H, N, [], []) :- H2 is H + H, H2 >= N.
-calculo_multiplos(H, N, [], L) :- H2 is H + H, H2 < N, calculo_multiplos(H, N, [H2], L).
-calculo_multiplos(H, N, [H1|L1], L) :- H2 is H1 + H, H2 < N, calculo_multiplos(H, N, [H2,H1|L1], L).
-calculo_multiplos(H, N, [H1|L1], [H1|L1]) :- H2 is H1 + H, H2 >= N.
-
 % depurarH(+H, +L, ?L2). <- La lista L2 resulta de eliminar todos los numeros multiplos de H en la lista L
 depurarH(_, _, [], []).
-depurarH(H, N, [H1|L], L2) :- calculo_multiplos(H, N, [], M), borrar(M, [H1|L], L2).
-
-% IDEA
-% depurarH(H, N, [H1|L], L2) :- 0 is mod(H1, H), depurarH(H, N, L, L2).
-% depurarH(H, N, [H1|L], [H1|L2]) :- not(0 is mod(H1, H)), depurarH(H, N, L, L2).
+depurarH(H, N, [H1|L], L2) :- 0 is mod(H1, H), depurarH(H, N, L, L2).
+depurarH(H, N, [H1|L], [H1|L2]) :- not(0 is mod(H1, H)), depurarH(H, N, L, L2).
 
 % depurar(+L1, ?L2) <- La lista L2 es la lista L1 que solo contiene a los primos.
 depurar(N, [H|L], [H|L]) :- H2 is H * H,  not(H2 < N).
